827ed93f7056c2443975e061349f561ea90eeb32
ked-m <ked-mic@wp.pl>
2020-11-22 20:43:34 +0100

    Initial commit

---
+++ lab5/2d_paint.py
@@ -0,0 +1,99 @@
+from lab5.shape_classes import *
+
+
+class Paint:
+    possible_actions = ["set_background_color", "set_border_color", "rotate", "move", "scale"]
+    figures = {"triangle": Triangle, "rectangle": Rectangle, "square": Square, "circle": Circle}
+
+    def __init__(self):
+        self.names = {}
+
+    def add(self, figure, name, *size):
+        if figure in Paint.figures.keys():
+            if name not in self.names.keys():
+                try:
+                    self.names[name] = Paint.figures[figure](name, *size)
+                except:
+                    print("Wrong size!")
+            else:
+                print("Name already taken or wrong name")
+        else:
+                print("Unknown figure type")
+
+    def remove(self, name):
+        if name in self.names.keys():
+            del self.names[name]
+        else:
+            print("Unknown name")
+
+    def command_line(self):
+        while True:
+            command = input("Enter a command: ")
+            command = command.lower()
+
+            if command == "quit":
+                for figure in self.names.values():
+                    print(figure)
+                break
+
+            command = command.split()
+            if len(command) >= 4 and command[0] == "add":
+                self.add(command[1], command[2], *command[3:])
+            elif len(command) == 4 and command[0] == "move":
+                if command[1] in self.names.keys():
+                    try:
+                        self.names[command[1]].move(float(command[2]), float(command[3]))
+                    except:
+                        print("Vector cant be a string")
+                else:
+                    print("Unknown name")
+            elif len(command) == 3 and command[0] in Paint.possible_actions:
+                if command[1] in self.names.keys():
+                    if command[0] == "set_border_color":
+                        self.names[command[1]].set_border_color(command[2])
+                    elif command[0] == "set_background_color":
+                        self.names[command[1]].set_background_color(command[2])
+                    elif command[0] == "scale":
+                        try:
+                            self.names[command[1]].scale(float(command[2]))
+                        except:
+                            print("Ratio must be a float")
+                    elif command[0] == "rotate":
+                        try:
+                            self.names[command[1]].rotate(float(command[2]))
+                        except:
+                            print("Angle must be a float")
+
+                else:
+                    print("Unknown name")
+
+            elif len(command) == 2 and command[0] == "remove":
+                self.remove(command[1])
+
+            elif len(command) == 1 and command[0] == "help":
+                print("""Polecenia:
+                    add <figure> <name> <size>
+                    remove <name>
+                    move <name> <vector>
+                    scale <name> <ratio>
+                    rotate <name> <angle>
+                    set_border_color <name> <color>
+                    set_background_color <name> <color>
+                    help
+                    quit
+
+
+                <figure> to jedno z: circle, square, rectangle, triangle
+                <name> - dowolny unikatowy identyfikator mogący zawierać litery, cyfry i podkreślniki, nie zaczynający się od cyfry
+                <ratio> - dowolna liczba rzeczywista, różna od 0
+                <angle> - dowolny kąt w stopniach
+                <color> to jedno z: black, white, red, green, blue, cyan, magenta, yellow
+                Każda figura po dodaniu ma środek w punkcie (0, 0).""")
+
+            else:
+                print("Unknown command")
+
+
+if __name__ == "__main__":
+    c1 = Paint()
+    c1.command_line()
---
+++ lab5/shape_classes.py
@@ -0,0 +1,118 @@
+def is_number(number):
+    try:
+        float(number)
+        return True
+    except:
+        return False
+
+
+class Vector_2D:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector_2D(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __str__(self):
+        return "[" + str(self.x) + ", " + str(self.y) + "]"
+
+
+class Shape:
+    color = ["black", "white", "red", "green", "blue", "cyan", "magenta", "yellow"]
+
+    def __init__(self, name, *size):
+        self.name = name
+        self.spot = Vector_2D(0, 0)
+        self.angle = 0
+        self.size = [float(i) for i in size]
+        self.border_color = "black"
+        self.background_color = "white"
+
+    def set_background_color(self, color):
+        if color in Shape.color:
+            self.background_color = color
+        else:
+            print("Color is not available")
+
+    def set_border_color(self, color):
+        if color in Shape.color:
+            self.border_color = color
+        else:
+            print("Color is not available")
+
+    def move(self, x, y):
+        if is_number(x) and is_number(y):
+            self.spot += Vector_2D(x, y)
+
+    def rotate(self, degrees):
+        if is_number(degrees):
+            self.angle += degrees
+            while self.angle > 360:
+                self.angle -= 360
+        else:
+            print('Degrees must be a float')
+
+    def scale(self, ratio):
+        if is_number(ratio):
+            if ratio != 0:
+                self.size = [i * ratio for i in self.size]
+            else:
+                print("Ratio cant be 0!")
+        else:
+            print('Ratio must be a float')
+
+    def __str__(self):
+        return "name: {}, border color: {}, background color: {}, angle: {} degrees, spot: {}, size: {}".format(self.name, self.border_color,
+                                                                                                                self.background_color, self.angle, self.spot, self.size)
+
+
+class Circle(Shape):
+
+    def __init__(self, name, *size):
+        super().__init__(name, *size)
+        if len(size) != 1:
+            raise ValueError
+
+    def __str__(self):
+        return "Figure: circle, " + super().__str__()
+
+
+class Square(Shape):
+
+    def __init__(self, name, *size):
+        super().__init__(name, *size)
+        if len(size) != 1:
+            raise ValueError
+
+    def __str__(self):
+        return "Figure: square, " + super().__str__()
+
+
+class Triangle(Shape):
+
+    def __init__(self, name, *size):
+        super().__init__(name, *size)
+        if len(size) != 3:
+            raise ValueError
+
+        if self.size[0] + self.size[1] <= self.size[2] or self.size[1] + self.size[2] <= self.size[0] or self.size[0] + self.size[2] <= self.size[1]:
+            raise ValueError
+
+    def __str__(self):
+        return "Figure: triangle, " + super().__str__()
+
+
+class Rectangle(Shape):
+
+    def __init__(self, name, *size):
+        super().__init__(name, *size)
+        if len(size) != 2:
+            raise ValueError
+
+    def __str__(self):
+        return "Figure: rectangle, " + super().__str__()
