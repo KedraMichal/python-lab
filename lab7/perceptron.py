3c483802b5ca1b0d0f38cd36f1a87d7acae35c91
ked-m <ked-mic@wp.pl>
2020-12-07 10:33:39 +0100

    Initial commit

--- lab7/perceptron.py
+++ lab7/perceptron.py
@@ -8,12 +8,10 @@ class Perceptron:
         self.x = x
         self.label = label
         self.weights = np.zeros(data.shape[1])
-        self.bias = np.zeros(data.shape[1])
+        self.bias = np.array(0)
 
     def test(self):
-        self.output = np.dot(self.x, self.weights.T) + sum(self.bias)
-        print(self.output.shape)
-        print(self.output)
+        self.output = np.dot(self.x, self.weights.T) + self.bias
         for ind, value in enumerate(self.output):
             if value > 1:
                 self.output[ind] = 1
@@ -23,20 +21,18 @@ class Perceptron:
         diff = self.label - self.output
         self.error = np.sum(diff)
 
-    #nie działa
-    def train(self):
-        for ind, row in enumerate(self.x):
-            a = np.dot(row, self.weights) + np.sum(self.bias)
-            if self.label[ind] == 0:
-                y = -1
-            else:
-                y = 1
-            print(self.bias)
-            print(self.weights)
-            if a*y <= 0:
-                for i in range(len(self.weights)):
-                    self.weights[i] = self.weights[i] + row[i] * y
-                    self.bias[i] = self.bias[i] - y
+    def train(self, epochs):
+        for i in range(epochs):
+            for ind, row in enumerate(self.x):
+                a = np.dot(row, self.weights) + np.sum(self.bias)
+                if self.label[ind] == 0:
+                    y = -1
+                else:
+                    y = 1
+                if a*y <= 0:
+                    for i in range(len(self.weights)):
+                        self.weights[i] = self.weights[i] + row[i] * y
+                        self.bias = self.bias - y
 
 
 if __name__ == "__main__":
@@ -44,4 +40,7 @@ if __name__ == "__main__":
     x, label = choose_label(data, 16)
     p1 = Perceptron(data, label)
     p1.test()
-    print(p1.error)
+    print("Error is: {}".format(p1.error))
+    p1.train(epochs=3)
+    p1.test()
+    print("Error is: {}".format(p1.error))
